b7a656486527f9b42d4c20d60ced2b5ac32a3c0e
Mahesh Kulkarni <maheshkulkarni@Maheshs-MacBook-Pro.local>
2022-04-30 23:41:17 +0530

    add new Xcode project without gitignore

---
+++ NavigationController/BasicNavigationController/BasicNavigationController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicNavigationController
+//
+//  Created by Mahesh Kulkarni on 30/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("heloow")
+    }
+
+
+}
+
