01630d35695ed51c5ea472231291bc1c62fa2c73
Mahesh Kulkarni <maheshkulkarni@Maheshs-MacBook-Pro.local>
2022-07-02 23:51:59 +0530

    add pagination

--- MVVM-Course/AirLineApp/AirLineApp/View/Controller/ViewController.swift
+++ MVVM-Course/AirLineApp/AirLineApp/View/Controller/ViewController.swift
@@ -10,19 +10,37 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var airLinetblView: UITableView!
-    
+    var currentPageNo = 0
     var viewModel = AllPassengerViewModel()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         self.title = "AirLine List"
-        viewModel.viewController = self
         registerPassengerTableViewCell()
-        viewModel.getAllPassengers(pageNo: viewModel.currentPageNo, pageSize: Constant.pageSize)
+        viewModel.getAllPassengers(pageNo: currentPageNo,pagination: false) { [weak self] result in
+            switch result {
+            case .success(let response):
+                self?.viewModel.passengerModelArray.append(contentsOf: response)
+                DispatchQueue.main.async {
+                self?.airLinetblView.reloadData()
+                }
+            case .failure(_):
+                break
+            }
+        }
     }
     
     func registerPassengerTableViewCell() {
         airLinetblView.register(PassengerTableViewCell.nib(), forCellReuseIdentifier: PassengerTableViewCell.cellIdentifier)
+    }
+    
+    func createSpinnerFooter() -> UIView {
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 100))
+        let spinner = UIActivityIndicatorView()
+        spinner.center = footerView.center
+        footerView.addSubview(spinner)
+        spinner.startAnimating()
+        return footerView
     }
     
     
@@ -41,5 +59,39 @@ extension ViewController: UITableViewDataSource,UITableViewDelegate {
         cell.configureAirLineData(model: airLine)
         return cell
     }
+}
+
+extension ViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        // calculates where the user is in the y-axis
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = airLinetblView.contentSize.height
+        
+        if offsetY > (contentHeight - 100 - scrollView.frame.size.height) {
+            print("fetch more")
+            guard !viewModel.isPagination else {
+                return
+            }
+            self.airLinetblView.tableFooterView = createSpinnerFooter()
+            currentPageNo += 1
+            viewModel.getAllPassengers(pageNo: currentPageNo, pagination: true) { [weak self]  result in
+                DispatchQueue.main.async {
+                    self?.airLinetblView.tableFooterView = nil
+                }
+                switch result {
+                case .success(let response):
+                    self?.viewModel.passengerModelArray.append(contentsOf: response)
+                    DispatchQueue.main.async {
+                        self?.airLinetblView.reloadData()
+                    }
+                case .failure(_):
+                    break
+                }
+            }
+        }
+    }
+
 }
 
--- MVVM-Course/AirLineApp/AirLineApp/ViewModel/AllPassengerViewModel.swift
+++ MVVM-Course/AirLineApp/AirLineApp/ViewModel/AllPassengerViewModel.swift
@@ -11,29 +11,32 @@ class AllPassengerViewModel {
     
     var passengerModelArray = [PassengerModel]()
     var airLineModelArray = [AirLineModel]()
-    weak var viewController:ViewController?
-    var currentPageNo = 0
+    var isPagination = false
     
-    func getAllPassengers(pageNo:Int,pageSize:Int)  {
-        let getAllPassengerUrlStr = "https://api.instantwebtools.net/v1/passenger?page=\(pageNo)&size=\(pageSize)"
+    func getAllPassengers(pageNo:Int,pagination: Bool,completion:@escaping ((Result<[PassengerModel],Error>) -> Void))  {
+        let getAllPassengerUrlStr = "https://api.instantwebtools.net/v1/passenger?page=\(pageNo)&size=\(Constant.pageSize)"
         guard let url = URL(string: getAllPassengerUrlStr) else { return }
         
+        if pagination {
+            self.isPagination = true
+        }
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
                 print("Error with fetching films: \(error)")
+                completion(.failure(error))
                 return
             }
             if let data = data {
                 do {
                     let allPassenger = try JSONDecoder().decode(AllPassengersModel.self, from: data)
-                    self.passengerModelArray.append(contentsOf: allPassenger.data)
-                    print(self.passengerModelArray)
-                    DispatchQueue.main.async {
-                        self.viewController?.airLinetblView.reloadData()
+                    completion(.success(allPassenger.data))
+                    if pagination {
+                        self.isPagination = false
                     }
                     
                 } catch(let error) {
                     print(error)
+                    completion(.failure(error))
                 }
             }
         } .resume()
