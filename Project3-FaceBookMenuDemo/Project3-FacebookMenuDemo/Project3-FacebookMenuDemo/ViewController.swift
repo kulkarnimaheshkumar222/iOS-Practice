bce15ea3585b81ee3491cf4edaa7eb71a83be1c3
Mahesh Kulkarni <kulkarnimaheshkumar222@gmail.com>
2024-07-13 21:54:42 +0530

    add collection view and it's basic datasource methods

--- Project3-FaceBookMenuDemo/Project3-FacebookMenuDemo/Project3-FacebookMenuDemo/ViewController.swift
+++ Project3-FaceBookMenuDemo/Project3-FacebookMenuDemo/Project3-FacebookMenuDemo/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
+}
 
-
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? UICollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.backgroundColor = .red
+        return cell
+    }
 }
 
